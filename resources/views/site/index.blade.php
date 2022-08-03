c2243b2b8347737e0cab3f7190bed4f7da42cfc6
ulaskorpe <ulaskorpe@gmail.com>
2022-08-03 08:43:05 +0000

    frontend libraries

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,36 +1,8 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- basic -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- mobile metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-    <!-- site metas -->
-    <title>diigo</title>
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- bootstrap css -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- style css -->
-    <link rel="stylesheet" href="css/style.css">
-    <!-- Responsive-->
-    <link rel="stylesheet" href="css/responsive.css">
-    <!-- fevicon -->
-    <link rel="icon" href="images/fevicon.png" type="image/gif" />
-    <!-- Scrollbar Custom CSS -->
-    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
-    <!-- Tweaks for older IEs-->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-</head>
+<html lang="tr">
+@include('site.partials.head')
 <!-- body -->
 <body class="main-layout">
 <!-- loader  -->
@@ -256,7 +228,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <p>Copyright 2019 All Right Reserved By <a href="https://html.design/"> Free  html Templates</a></p>
+                        <p>Copyright 2022 All Right Reserved By MERT MARTI</p>
                     </div>
                 </div>
             </div>
@@ -264,16 +236,7 @@
     </div>
 </footer>
 <!-- end footer -->
-<!-- Javascript files-->
-<script src="js/jquery.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/jquery-3.0.0.min.js"></script>
-<script src="js/plugin.js"></script>
-<!-- sidebar -->
-<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-<script src="js/custom.js"></script>
-<script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+@include('site.partials.scripts')
 </body>
 </html>
 
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[\App\Http\Controllers\HomeController::class,'home'])->name('home');
+
+
+Route::group(['middleware'=>\App\Http\Middleware\checkUser::class,'prefix'=>'users'],function (){
+
+});
+
+
+Route::group(['prefix'=>'admin' ],function (){
+    Route::get('/login',[\App\Http\Controllers\AdminController::class,'login'])->name('admin.login');
+    Route::post('/login',[\App\Http\Controllers\AdminController::class,'loginPost'])->name('admin.login-post');
+    Route::group(['middleware'=>\App\Http\Middleware\checkAdmin::class],function (){
+        Route::get('/',[\App\Http\Controllers\AdminController::class,'index'])->name('admin.index');
+        Route::post('/logout',[\App\Http\Controllers\AdminController::class,'logout'])->name('logout');
+
+    });
+
+});
+
